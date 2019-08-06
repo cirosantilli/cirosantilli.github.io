23125eb458db647b54b0f82846051ecd943b46f9
Ciro Santilli <ciro.santilli@gmail.com>
2019-08-06 00:00:00 +0000

    katex plugin working on block blockmacro and inline!
    TODO:
    
    - docinfo processor to dispense using docinfo
    - macros across blocks
    - Equation #num prefix to descriptions

--- katex.rb
+++ katex.rb
@@ -1,5 +1,58 @@
 #!/usr/bin/env ruby
 
+<<-HEREDOC
+Asciidoctor extension to render Katex mathematics server side to HTML fast with Schmooze: https://github.com/Shopify/schmooze
+
+Usage:
+
+README.adoc
+
+....
+:docinfo: shared
+
+Inline katex:[\sqrt{1+1}] katex
+
+Block one-liner:
+
+katex::[\sqrt{1+1}]
+
+Block multi-liner: xref:math-test-math[]
+
+[katex,id=math-test-math]
+.A test block equation
+[katex]
+....
+\sqrt{1+1} \\
+\sqrt{1+1}
+....
+
+docinfo.html
+
+....
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.css"
+  crossorigin="anonymous"
+  integrity="sha384-yFRtMMDnQtDRO8rLpMIKrtPCD5jdktao2TV19YiZYWMDkUR5GQZR/NOVTdquEx1j"
+>
+<style>
+.katex { font-size: 1.5em; }
+</style>
+....
+
+Istall katex:
+
+....
+npm install -g katex@0.10.2
+....
+
+Use the extension:
+
+....
+asciidoctor -r katex.rb README.adoc
+....
+HEREDOC
+
 require 'asciidoctor'
 require 'asciidoctor/extensions'
 require 'schmooze'
@@ -9,47 +62,58 @@ class KatexSchmoozer < Schmooze::Base
   method :renderToString, 'katex.renderToString'
 end
 $katex = KatexSchmoozer.new(__dir__)
+$macros = {}
 
-class KatexInline < Asciidoctor::Extensions::InlineMacroProcessor
+class KatexBlockProcessor < Asciidoctor::Extensions::BlockProcessor
   use_dsl
-  named :latexmath
-
-  def process parent, target, attrs
-    # TODO never gets called on latexmath: but gets called on latexmath::. TODO what is the difference?
-    asdf
-    text = attrs[1]
-    if text.nil? || text.empty?
-      text = target
-    end
-    if !ExternalLinkRegex.match?(target)
-      target = File.join(target_base, target)
-    end
-    $katex.renderToString("\\sqrt{1+1} = 2", {throwOnError: false})
-    create_anchor parent, text, type: :link, target: target
+  named :katex
+  on_context :literal
+  parse_content_as :raw
+  def process parent, reader, attrs
+    content = $katex.renderToString(
+      reader.lines.join("\n"),
+      {
+        macros: $macros,
+        throwOnError: true
+      }
+    )
+    create_paragraph parent, content, attrs, subs: nil
   end
 end
 
-class KatexBlock < Asciidoctor::Extensions::TreeProcessor
-  def process document
-    return unless document.blocks?
-    process_blocks document
-    nil
+class KatexBlockMacroProcessor < Asciidoctor::Extensions::BlockMacroProcessor
+  use_dsl
+  named :katex
+  def process parent, target, attrs
+    content = $katex.renderToString(
+      attrs[1],
+      {
+        macros: $macros,
+        throwOnError: true
+      }
+    )
+    create_paragraph parent, content, attrs, subs: nil
   end
+end
 
-  def process_blocks node
-    node.blocks.each_with_index do |block, i|
-      if block.context == :stem and block.style == "latexmath"
-        # TODO getting called, and I have the content, now I need to replace it.
-        #puts block
-        #puts block.content
-        #puts $katex.renderToString(block.content, {throwOnError: false})
-      end
-      process_blocks block if block.blocks?
-    end
+class KatexInlineMacroProcessor < Asciidoctor::Extensions::InlineMacroProcessor
+  use_dsl
+  named :katex
+  using_format :short
+  def process parent, target, attrs
+    html = $katex.renderToString(
+      attrs[1],
+      {
+        macros: $macros,
+        throwOnError: true
+      }
+    )
+    create_inline parent, :quoted, html
   end
 end
 
 Asciidoctor::Extensions.register do
-  inline_macro KatexInline
-  treeprocessor KatexBlock
+  block KatexBlockProcessor
+  block_macro KatexBlockMacroProcessor
+  inline_macro KatexInlineMacroProcessor
 end
