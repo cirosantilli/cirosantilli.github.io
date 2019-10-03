0f78e139813c9c0f2281ceec13e6ece8fb2dd254
Ciro Santilli <ciro.santilli@gmail.com>
2019-10-03 00:00:01 +0000

    xref2: also working for non-headers

--- _plugins/asciidoctor-extensions.rb
+++ _plugins/asciidoctor-extensions.rb
@@ -2,31 +2,37 @@ require 'asciidoctor/extensions'
 
 require 'sqlite3'
 
-# Cross reference database.
 $PROJECT_ROOT = File.dirname(File.dirname(__FILE__))
-$DB_DIR = File.join($PROJECT_ROOT, 'out')
-$DB_PATH = File.join($DB_DIR, 'db.sqlite')
-FileUtils.mkdir_p $DB_DIR
-$DB = SQLite3::Database.new $DB_PATH
-$DB_TABLE_NAME = 'sections'
-# The extension this character for for all OSes.
-$PATH_SEPARATOR = '/'
-# Check if table exists and create it if it doesn't.
-if $DB.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='#{$DB_TABLE_NAME}';").empty?
-  $DB.execute <<-SQL
-    CREATE TABLE '#{$DB_TABLE_NAME}' (
-      path TEXT,
-      level INT,
-      id TEXT,
-      title TEXT
-    );
-  SQL
-end
-$DB_FAIL_ON_MISSING_REF = ENV['CIROSANTILLI_COM_DB_FAIL_ON_MISSING_REF'] == '1'
 
 def db_root_relpath path
   f = Pathname.new(path).relative_path_from($PROJECT_ROOT)
   File.basename(f,File.extname(f))
+end
+
+# xref2 cross reference database.
+$XREF2_FAIL_ON_MISSING_REF = ENV['CIROSANTILLI_COM_XREF2_FAIL_ON_MISSING_REF'] == '1'
+$XREF2_LOCAL = ENV['CIROSANTILLI_COM_XREF2_LOCAL'] == '1'
+if $XREF2_LOCAL
+  $XREF2_LOCAL_EXT = '.html'
+else
+  $XREF2_LOCAL_EXT = ''
+end
+$XREF2_DB_DIR = File.join($PROJECT_ROOT, 'out')
+$XREF2_DB_PATH = File.join($XREF2_DB_DIR, 'db.sqlite')
+FileUtils.mkdir_p $XREF2_DB_DIR
+$XREF2_DB = SQLite3::Database.new $XREF2_DB_PATH
+$XREF2_DB_TABLE_NAME = 'sections'
+# The extension this character for for all OSes.
+$XREF2_PATH_SEPARATOR = '/'
+# Check if table exists and create it if it doesn't.
+if $XREF2_DB.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='#{$XREF2_DB_TABLE_NAME}';").empty?
+  $XREF2_DB.execute <<-SQL
+    CREATE TABLE '#{$XREF2_DB_TABLE_NAME}' (
+      path TEXT,
+      id TEXT,
+      title TEXT
+    );
+  SQL
 end
 
 ## Wikimedia stuff.
@@ -213,31 +219,36 @@ class WikimediaVideo2BlockProcessor < Video2BlockProcessor
   end
 end
 
-# Dump IDs into a database so we can xref2 to it across files.
+# Dump IDs of document elements such as headers, images, etc. into
+# a database file so we can xref2 to it across files.
 #
 # For debugging, debug the database with: `sqlite3 out/db.sqlite .dump`
-class ExtractHeaderIds < Asciidoctor::Extensions::TreeProcessor
+#
+# Bibliography:
+#
+# * https://github.com/asciidoctor/asciidoctor/issues/2500
+class Xref2ExtractIdsToSqlite < Asciidoctor::Extensions::TreeProcessor
   def process document
+    # jekyll-asciidoctor does a header only pass which invokes this though,
+    # so we have to skip that one or else it resets the XREF2_DB to contain the top header only.
     if not document.options[:parse_header_only]
-      # Drop all entries from the current file. We are going to reprocess the file,
-      # so we want to catch entries that have been removed.
-      #
-      # jekyll-asciidoctor does a header only pass which invokes this though,
-      # so we have to skip that one or else it resets the DB to contain the top header only.
       path = db_root_relpath document.attributes['docfile']
-      $DB.execute "DELETE FROM '#{$DB_TABLE_NAME}' WHERE path = '#{path}'"
-      (document.find_by context: :section).each do |section|
-        # The toplevel header does not have an ID.
-        if section.level == 0
-          id = Asciidoctor::Section.generate_id(document.attributes['docname'], document)
-        else
-          id = section.id
-        end
-        $DB.execute "INSERT INTO #{$DB_TABLE_NAME} VALUES (?, ?, ?, ?)",
-          [path.to_s, section.level, id, section.title]
+      # First drop all entries from the current file. We are going to reprocess the file,
+      # so we want to catch entries that have been removed.
+      $XREF2_DB.execute "DELETE FROM '#{$XREF2_DB_TABLE_NAME}' WHERE path = '#{path}'"
+      # Add an entry for the toplevel.
+      insert_into_db path.to_s, path.to_s, document.title
+      # And now add entries for every document element.
+      document.catalog[:refs].each do |key, ref|
+        insert_into_db path.to_s, ref.id, ref.title
       end
     end
     nil
+  end
+
+  def insert_into_db path, id, title
+    $XREF2_DB.execute "INSERT INTO #{$XREF2_DB_TABLE_NAME} VALUES (?, ?, ?)",
+                [path, id, title]
   end
 end
 
@@ -247,43 +258,50 @@ class Xref2InlineMacroProcessor < Asciidoctor::Extensions::InlineMacroProcessor
   named :xref2
 
   def process parent, target, attrs
-    target_split = target.split($PATH_SEPARATOR)
+    target_split = target.split($XREF2_PATH_SEPARATOR)
     if target_split.length > 1
       # In another file.
-      target_file = target_split[0, target_split.length - 1].join($PATH_SEPARATOR)
+      target_file = target_split[0, target_split.length - 1].join($XREF2_PATH_SEPARATOR)
       target_id = target_split[-1]
-      href = "#{target_file}##{target_id}"
-      rows = $DB.execute(
-        "SELECT * FROM #{$DB_TABLE_NAME} " \
+      href = "#{target_file}#{$XREF2_LOCAL_EXT}##{target_id}"
+      rows = $XREF2_DB.execute(
+        "SELECT * FROM #{$XREF2_DB_TABLE_NAME} " \
         "WHERE path = '#{target_file}' " \
         "AND id = '#{target_id}';"
       )
     else
       # In the current file or a toplevel header in another file?
       # First try toplevel header in another file.
-      rows = $DB.execute(
-        "SELECT * FROM #{$DB_TABLE_NAME} " \
-        "WHERE path = '#{target}' " \
-        "AND level = 0;"
+      rows = $XREF2_DB.execute(
+        "SELECT * FROM #{$XREF2_DB_TABLE_NAME} " \
+        "WHERE path = '#{target}' "
       )
-      href = target
-      if rows.length == 0
-        # If not found, then try a header in the current file.
-        current_file = db_root_relpath parent.document.attributes['docfile']
-        rows = $DB.execute(
-          "SELECT * FROM #{$DB_TABLE_NAME} " \
+      if rows.length > 0
+        # Yup, toplevel header of another file.
+        href = "#{target}#{$XREF2_LOCAL_EXT}"
+      else
+        # Header in current file.
+        current_file = db_root_relpath(parent.document.attributes['docfile'])
+        rows = $XREF2_DB.execute(
+          "SELECT * FROM #{$XREF2_DB_TABLE_NAME} " \
           "WHERE path = '#{current_file}' " \
-          "AND id = '#{target}';"
+          "AND id = '#{target}' "
         )
         href = "##{target}"
       end
     end
     if rows.length > 0
-      path, level, id, target_text = rows[0]
-      if attrs.key?(1)
+      path, id, target_text = rows[0]
+      if attrs.key? 1
         text = attrs[1]
       else
         text = target_text
+        if attrs.key? 'xrefstyle'
+          if attrs['xrefstyle'] == 'full'
+            # TODO duplicates AbstractBlock::xreftext a bit, any way to factor out?
+            # Add number and type here.
+          end
+        end
       end
     else
       warn = "reference not found: #{target}"
@@ -303,6 +321,6 @@ Asciidoctor::Extensions.register do
   block_macro Video2BlockProcessor
   block_macro WikimediaImage2BlockProcessor
   block_macro WikimediaVideo2BlockProcessor
-  treeprocessor ExtractHeaderIds
+  treeprocessor Xref2ExtractIdsToSqlite
   inline_macro Xref2InlineMacroProcessor
 end
