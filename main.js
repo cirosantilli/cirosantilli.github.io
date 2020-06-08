f8b63c6a35b94faf4a5c8b14c9d26ec156330671
Ciro Santilli <ciro.santilli@gmail.com>
2020-06-08 07:00:06 +0000

    main.js to cirodown main

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-<!-- Google Analytics, AKA selling my soul to Google for some backlinks. -->
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-47867706-1', 'auto');
-ga('send', 'pageview');
-
 // Load required scripts dynamically:
 //
 // * https://stackoverflow.com/questions/7308908/waiting-for-dynamically-loaded-script/57267538#57267538
