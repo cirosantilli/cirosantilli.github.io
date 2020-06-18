c0b9ad7d3241b3c7043a60924e1d2bc9937d8646
Ciro Santilli <ciro.santilli@gmail.com>
2020-06-18 01:00:00 +0000

    tablesort example

--- main.js
+++ main.js
@@ -248,15 +248,3 @@ class CirosantilliCanvasDemo {
         this.height = canvas_width
     }
 }
-
-/* TODO coffesscript on old website. Still needed? */
-/*
-$ ->
-  $('h2, h3, h4, h5, h6').each (i, el) ->
-    el = $(el)
-    id = el.attr('id')
-    icon = '<i class="fa fa-link"></i>'
-    if id
-      el.append(' ', $('<a>').addClass('header-link').
-        attr('href', '#' + id).html(icon))
-*/
