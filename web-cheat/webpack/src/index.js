14f2c02c777e5429e3225398e56d370bb20fecdd
Ciro Santilli <ciro.santilli@gmail.com>
2021-06-04 18:20:08 +0100

    948fc8a47862c096d8f09c7129d727171964bd36

--- web-cheat/webpack/src/index.js
+++ web-cheat/webpack/src/index.js
@@ -5,11 +5,7 @@ import { notindex_constant } from './notindex.js';
 
 function component() {
   const element = document.createElement('div');
-
-// Lodash, currently included via a script, is required for this line to work
-// Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack', notindex_constant], ' ');
-
   return element;
 }
 
