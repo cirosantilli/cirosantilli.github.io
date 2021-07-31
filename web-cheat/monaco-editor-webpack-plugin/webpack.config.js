8ed9720d2a6045e751960f1427196b99965d9a79
Ciro Santilli <ciro.santilli@gmail.com>
2021-07-31 22:12:06 +0100

    publish web-cheat htmls except webpack

---
+++ web-cheat/monaco-editor-webpack-plugin/webpack.config.js
@@ -0,0 +1,22 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.ttf$/,
+      use: ['file-loader']
+    }]
+  },
+  plugins: [
+    new MonacoWebpackPlugin()
+  ]
+};
