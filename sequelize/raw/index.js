7c9873e193e90251a27209444de21f6433fb0440
Ciro Santilli <ciro.santilli@gmail.com>
2021-11-03 22:32:08 +0000

    improve sequelize a bit

--- sequelize/raw/index.js
+++ sequelize/raw/index.js
@@ -14,11 +14,16 @@ CREATE TABLE IntegerNames (
   name TEXT NOT NULL
 )
 `)
-await Promise.all([
-  `INSERT INTO IntegerNames VALUES (NULL, 2, 'two')`,
-  `INSERT INTO IntegerNames VALUES (NULL, 3, 'three')`,
-  `INSERT INTO IntegerNames VALUES (NULL, 5, 'five')`,
-].map(s => sequelize.query(s)))
+await sequelize.query(`CREATE INDEX valueIdx ON IntegerNames (value)`)
+async function reset() {
+  await sequelize.query(`DELETE FROM IntegerNames`)
+  return Promise.all([
+    `INSERT INTO IntegerNames VALUES (NULL, 2, 'two')`,
+    `INSERT INTO IntegerNames VALUES (NULL, 3, 'three')`,
+    `INSERT INTO IntegerNames VALUES (NULL, 5, 'five')`,
+  ].map(s => sequelize.query(s)))
+}
+await reset()
 
 let rows, meta
 
@@ -49,7 +54,7 @@ WHERE value > 2
 assert.strictEqual(rows[0].count, 2)
 assert.strictEqual(rows.length, 1)
 
-// SUM aggregate.
+// SUM aggregate query.
 ;[rows, meta] = await sequelize.query(`SELECT SUM(value) as 'sum' FROM IntegerNames`)
 assert.strictEqual(rows[0].sum, 10)
 assert.strictEqual(rows.length, 1)
@@ -64,8 +69,7 @@ assert.strictEqual(rows[1].name, 'three')
 assert.strictEqual(rows[2].value, 55)
 assert.strictEqual(rows[2].name, 'five')
 assert.strictEqual(rows.length, 3)
-// Undo.
-await sequelize.query(`UPDATE IntegerNames SET value = 5 WHERE value = 55`)
+await reset()
 
 // UPDATE multiple rows.
 await sequelize.query(`UPDATE IntegerNames SET value = value + 1`)
@@ -77,8 +81,7 @@ assert.strictEqual(rows[1].name, 'three')
 assert.strictEqual(rows[2].value, 6)
 assert.strictEqual(rows[2].name, 'five')
 assert.strictEqual(rows.length, 3)
-// Undo.
-await sequelize.query(`UPDATE IntegerNames SET value = value - 1`)
+await reset()
 
 // DELETE a row.
 await sequelize.query(`DELETE FROM IntegerNames WHERE value > 2`)
@@ -86,9 +89,7 @@ await sequelize.query(`DELETE FROM IntegerNames WHERE value > 2`)
 assert.strictEqual(rows[0].value, 2)
 assert.strictEqual(rows[0].name, 'two')
 assert.strictEqual(rows.length, 1)
-// Undo.
-await sequelize.query(`INSERT INTO IntegerNames VALUES (NULL, 3, 'three')`)
-await sequelize.query(`INSERT INTO IntegerNames VALUES (NULL, 5, 'five')`)
+await reset()
 
 await sequelize.close();
 })();
--- sequelize/raw/many_to_many.js
+++ sequelize/raw/many_to_many.js
@@ -22,22 +22,30 @@ CREATE TABLE AnimalTag (
 )
 `,
 ].map(s => sequelize.query(s)))
-await Promise.all([
-  `INSERT INTO Animal VALUES (0, 'dog')`,
-  `INSERT INTO Animal VALUES (1, 'cat')`,
-  `INSERT INTO Animal VALUES (2, 'hawk')`,
-  `INSERT INTO Animal VALUES (3, 'bee')`,
-  `INSERT INTO Tag VALUES (0, 'flying')`,
-  `INSERT INTO Tag VALUES (1, 'mammal')`,
-  `INSERT INTO Tag VALUES (2, 'vertebrate')`,
-  `INSERT INTO AnimalTag VALUES (0, 1)`,
-  `INSERT INTO AnimalTag VALUES (0, 2)`,
-  `INSERT INTO AnimalTag VALUES (1, 1)`,
-  `INSERT INTO AnimalTag VALUES (1, 2)`,
-  `INSERT INTO AnimalTag VALUES (2, 0)`,
-  `INSERT INTO AnimalTag VALUES (2, 2)`,
-  `INSERT INTO AnimalTag VALUES (3, 0)`,
-].map(s => sequelize.query(s)))
+async function reset() {
+  await Promise.all([
+    `DELETE FROM Animal`,
+    `DELETE FROM Tag`,
+    `DELETE FROM AnimalTag`,
+  ].map(s => sequelize.query(s)))
+  return Promise.all([
+    `INSERT INTO Animal VALUES (0, 'dog')`,
+    `INSERT INTO Animal VALUES (1, 'cat')`,
+    `INSERT INTO Animal VALUES (2, 'hawk')`,
+    `INSERT INTO Animal VALUES (3, 'bee')`,
+    `INSERT INTO Tag VALUES (0, 'flying')`,
+    `INSERT INTO Tag VALUES (1, 'mammal')`,
+    `INSERT INTO Tag VALUES (2, 'vertebrate')`,
+    `INSERT INTO AnimalTag VALUES (0, 1)`,
+    `INSERT INTO AnimalTag VALUES (0, 2)`,
+    `INSERT INTO AnimalTag VALUES (1, 1)`,
+    `INSERT INTO AnimalTag VALUES (1, 2)`,
+    `INSERT INTO AnimalTag VALUES (2, 0)`,
+    `INSERT INTO AnimalTag VALUES (2, 2)`,
+    `INSERT INTO AnimalTag VALUES (3, 0)`,
+  ].map(s => sequelize.query(s)))
+}
+await reset()
 
 let rows, meta
 
@@ -97,6 +105,31 @@ assert.strictEqual(rows[2].name, 'mammal')
 assert.strictEqual(rows[2].count, 2)
 assert.strictEqual(rows.length, 3)
 
+// Get animal counts for each tag that has less than 3 animals.
+// This illustrates HAVING, which is what you have to do when dealing with aggregations:
+// https://stackoverflow.com/questions/9253244/sql-having-vs-where
+;[rows, meta] = await sequelize.query(`
+SELECT
+  Tag.name AS name,
+  COUNT(*) AS count
+FROM Tag
+INNER JOIN AnimalTag
+  ON Tag.id = AnimalTag.tagId
+INNER JOIN Animal
+  ON AnimalTag.animalId = Animal.id
+GROUP BY Tag.id
+HAVING
+  count < 3
+ORDER BY
+  count DESC,
+  Tag.id ASC
+`)
+assert.strictEqual(rows[0].name, 'flying')
+assert.strictEqual(rows[0].count, 2)
+assert.strictEqual(rows[1].name, 'mammal')
+assert.strictEqual(rows[1].count, 2)
+assert.strictEqual(rows.length, 2)
+
 // Get animal counts only for the tags that are associated with 'dog'.
 ;[rows, meta] = await sequelize.query(`
 SELECT
@@ -121,6 +154,8 @@ assert.strictEqual(rows[1].name, 'mammal')
 assert.strictEqual(rows[1].count, 2)
 assert.strictEqual(rows.length, 2)
 
+// Queries that modify data.
+
 // ON DELETE CASCADE action: if we delete the 'vertebrate' tag,
 // corresponding relations are also deleted.
 await sequelize.query(`DELETE FROM Tag WHERE id = 2`)
@@ -137,11 +172,78 @@ assert.strictEqual(rows[2].tagId,    0)
 assert.strictEqual(rows[3].animalId, 3)
 assert.strictEqual(rows[3].tagId,    0)
 assert.strictEqual(rows.length, 4)
-// Undo.
-await sequelize.query(`INSERT INTO Tag VALUES (2, 'vertebrate')`)
-await sequelize.query(`INSERT INTO AnimalTag VALUES (0, 2)`)
-await sequelize.query(`INSERT INTO AnimalTag VALUES (1, 2)`)
-await sequelize.query(`INSERT INTO AnimalTag VALUES (2, 2)`)
+await reset()
+
+// DELETE all animals with a given tag.
+// DELETE + JOIN does not appear to be in the SQL standard, and sqlite 3.35 does not support it:
+// ``
+// near "INNER": syntax error
+// ``
+// so the only way is to do it with sub queries:
+// https://stackoverflow.com/questions/24511153/how-delete-table-inner-join-with-other-table-in-sqlite
+// PostgreSQL does support it however:
+// https://stackoverflow.com/questions/11753904/postgresql-delete-with-inner-join
+if (false) {
+  // JOIN version.
+  await sequelize.query(`
+DELETE FROM Animal
+INNER JOIN AnimalTag
+  ON Animal.id = AnimalTag.animalId
+INNER JOIN Tag
+  ON AnimalTag.tagId = Tag.id
+  AND Tag.name = 'flying'
+ORDER BY Animal.id ASC
+`)
+} else {
+  // Subquery version.
+  await sequelize.query(`
+DELETE FROM Animal
+WHERE Animal.id IN (
+  SELECT
+    Animal.id
+  FROM Animal
+  INNER JOIN AnimalTag
+    ON Animal.id = AnimalTag.animalId
+  INNER JOIN Tag
+    ON AnimalTag.tagId = Tag.id
+    AND Tag.name = 'flying'
+)
+`)
+}
+;[rows, meta] = await sequelize.query(`SELECT * FROM Animal ORDER BY id ASC`)
+assert.strictEqual(rows[0].name, 'dog')
+assert.strictEqual(rows[1].name, 'cat')
+assert.strictEqual(rows.length, 2)
+await reset()
+
+// Delete any tags associated with 'dog' that have less than 3 animals.
+// In our specific test database, this should delete only the "mammal" tag.
+// Application: with 1 instead of 3, we could use this to clean up possbly
+// empty tags after deleting animal, as there might be no more associated animals
+// to them after every animal deletion.
+await sequelize.query(`
+DELETE FROM Tag
+WHERE Tag.id IN (
+  SELECT
+    Tag.id
+  FROM Animal
+  INNER JOIN AnimalTag
+    ON Animal.name = 'dog'
+    AND Animal.id = AnimalTag.animalId
+  INNER JOIN Tag
+    ON Tag.id = AnimalTag.tagId
+  INNER JOIN AnimalTag as 'AnimalTag2'
+    ON AnimalTag2.tagId = AnimalTag.tagId
+  GROUP BY AnimalTag2.tagId
+  HAVING
+    COUNT(*) < 3
+)
+`)
+;[rows, meta] = await sequelize.query(`SELECT * FROM Tag ORDER BY id ASC`)
+assert.strictEqual(rows[0].name, 'flying')
+assert.strictEqual(rows[1].name, 'vertebrate')
+assert.strictEqual(rows.length, 2)
+await reset()
 
 await sequelize.close();
 })();
