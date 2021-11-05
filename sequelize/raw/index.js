4c55d313240023ff328cd62655ee726347821f88
Ciro Santilli <ciro.santilli@gmail.com>
2021-11-05 11:44:51 +0000

    get some raw sql working on both postgres and sqlite

--- sequelize/raw/index.js
+++ sequelize/raw/index.js
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
 // https://cirosantilli.com/sequelize
 const assert = require('assert');
-const path = require('path');
-const { Sequelize, DataTypes, Op } = require('sequelize');
-const sequelize = new Sequelize({ dialect: 'sqlite', storage: ':memory:' });
-(async () => {
+const { DataTypes, Op } = require('sequelize');
+const common = require('./common')
+const sequelize = common.sequelize(__filename)
+;(async () => {
 
 // Create tables and data.
+try { await sequelize.query(`DROP TABLE "IntegerNames"`) } catch (e) {}
 await sequelize.query(`
-CREATE TABLE IntegerNames (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE "IntegerNames" (
+  id INTEGER PRIMARY KEY,
   value INTEGER NOT NULL,
   name TEXT NOT NULL
 )
 `)
-await sequelize.query(`CREATE INDEX valueIdx ON IntegerNames (value)`)
+await sequelize.query(`CREATE INDEX "valueIdx" ON "IntegerNames" (value)`)
 async function reset() {
-  await sequelize.query(`DELETE FROM IntegerNames`)
+  await sequelize.query(`DELETE FROM "IntegerNames"`)
   return Promise.all([
-    `INSERT INTO IntegerNames VALUES (NULL, 2, 'two')`,
-    `INSERT INTO IntegerNames VALUES (NULL, 3, 'three')`,
-    `INSERT INTO IntegerNames VALUES (NULL, 5, 'five')`,
+    `INSERT INTO "IntegerNames" VALUES (0, 2, 'two')`,
+    `INSERT INTO "IntegerNames" VALUES (1, 3, 'three')`,
+    `INSERT INTO "IntegerNames" VALUES (2, 5, 'five')`,
   ].map(s => sequelize.query(s)))
 }
 await reset()
@@ -28,7 +29,7 @@ await reset()
 let rows, meta
 
 // Select all.
-;[rows, meta] = await sequelize.query(`SELECT * FROM IntegerNames ORDER BY value ASC`)
+;[rows, meta] = await sequelize.query(`SELECT * FROM "IntegerNames" ORDER BY value ASC`)
 assert.strictEqual(rows[0].value, 2)
 assert.strictEqual(rows[0].name, 'two')
 assert.strictEqual(rows[1].value, 3)
@@ -38,7 +39,7 @@ assert.strictEqual(rows[2].name, 'five')
 assert.strictEqual(rows.length, 3)
 
 // Select with a WHERE condition.
-;[rows, meta] = await sequelize.query(`SELECT * FROM IntegerNames WHERE value > 2 ORDER BY value ASC`)
+;[rows, meta] = await sequelize.query(`SELECT * FROM "IntegerNames" WHERE value > 2 ORDER BY value ASC`)
 assert.strictEqual(rows[0].value, 3)
 assert.strictEqual(rows[0].name, 'three')
 assert.strictEqual(rows[1].value, 5)
@@ -48,20 +49,33 @@ assert.strictEqual(rows.length, 2)
 // COUNT and rename.
 ;[rows, meta] = await sequelize.query(`
 SELECT COUNT(*) AS count
-FROM IntegerNames
+FROM "IntegerNames"
 WHERE value > 2
 `)
-assert.strictEqual(rows[0].count, 2)
+// PostgreSQL returns BIGINT, which is not widely available, so gets treated as a string. Thus we convert to integer ourselves.
+// https://stackoverflow.com/questions/47843370/postgres-sequelize-raw-query-to-get-count-returns-string-value
+assert.strictEqual(parseInt(rows[0].count, 10), 2)
 assert.strictEqual(rows.length, 1)
 
 // SUM aggregate query.
-;[rows, meta] = await sequelize.query(`SELECT SUM(value) as 'sum' FROM IntegerNames`)
-assert.strictEqual(rows[0].sum, 10)
+;[rows, meta] = await sequelize.query(`SELECT SUM(value) AS "sum" FROM "IntegerNames"`)
+assert.strictEqual(parseInt(rows[0].sum, 10), 10)
+assert.strictEqual(rows.length, 1)
+
+// Find the entire rows that reaches the MAX of the aggregate query.
+// We need the subquery as mentioned at:
+// https://www.postgresql.org/docs/14/tutorial-agg.html
+;[rows, meta] = await sequelize.query(`
+SELECT * FROM "IntegerNames" where value =
+  (SELECT MAX(value) FROM "IntegerNames")
+`)
+assert.strictEqual(rows[0].value, 5)
+assert.strictEqual(rows[0].name, 'five')
 assert.strictEqual(rows.length, 1)
 
 // UPDATE one row.
-await sequelize.query(`UPDATE IntegerNames SET value = 55 WHERE value = 5`)
-;[rows, meta] = await sequelize.query(`SELECT * FROM IntegerNames ORDER BY value ASC`)
+await sequelize.query(`UPDATE "IntegerNames" SET value = 55 WHERE value = 5`)
+;[rows, meta] = await sequelize.query(`SELECT * FROM "IntegerNames" ORDER BY value ASC`)
 assert.strictEqual(rows[0].value, 2)
 assert.strictEqual(rows[0].name, 'two')
 assert.strictEqual(rows[1].value, 3)
@@ -72,8 +86,8 @@ assert.strictEqual(rows.length, 3)
 await reset()
 
 // UPDATE multiple rows.
-await sequelize.query(`UPDATE IntegerNames SET value = value + 1`)
-;[rows, meta] = await sequelize.query(`SELECT * FROM IntegerNames ORDER BY value ASC`)
+await sequelize.query(`UPDATE "IntegerNames" SET value = value + 1`)
+;[rows, meta] = await sequelize.query(`SELECT * FROM "IntegerNames" ORDER BY value ASC`)
 assert.strictEqual(rows[0].value, 3)
 assert.strictEqual(rows[0].name, 'two')
 assert.strictEqual(rows[1].value, 4)
@@ -84,8 +98,8 @@ assert.strictEqual(rows.length, 3)
 await reset()
 
 // DELETE a row.
-await sequelize.query(`DELETE FROM IntegerNames WHERE value > 2`)
-;[rows, meta] = await sequelize.query(`SELECT * FROM IntegerNames ORDER BY value ASC`)
+await sequelize.query(`DELETE FROM "IntegerNames" WHERE value > 2`)
+;[rows, meta] = await sequelize.query(`SELECT * FROM "IntegerNames" ORDER BY value ASC`)
 assert.strictEqual(rows[0].value, 2)
 assert.strictEqual(rows[0].name, 'two')
 assert.strictEqual(rows.length, 1)
---
+++ sequelize/raw/parallel_update.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+// https://cirosantilli.com/sql-isolation-level-example
+
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const { DataTypes, Op } = require('sequelize');
+const common = require('./common');
+const sequelize = common.sequelize(__filename)
+
+;(async () => {
+if (isMainThread) {
+  const assert = require('assert');
+
+  // Create tables and data.
+  try { await sequelize.query(`DROP TABLE MyInt`) } catch (e) {}
+  await sequelize.query(`CREATE TABLE MyInt ( i INTEGER NOT NULL)`)
+  await sequelize.query(`INSERT INTO MyInt VALUES (0)`)
+  const threadCount = 2;
+  const incsPerThread = 10;
+  const threads = new Set();
+  for (let i = 0; i < threadCount; i++) {
+    threads.add(new Worker(__filename, { workerData: { incsPerThread } }));
+  }
+  for (let worker of threads) {
+    worker.on('error', err => { throw err; });
+    worker.on('exit', async () => {
+      threads.delete(worker);
+      if (threads.size === 0) {
+        //;[rows, meta] = await sequelize.query(`SELECT * FROM MyInt`)
+        //assert.strictEqual(rows[0].i, threadCount * incsPerThread)
+        await sequelize.close();
+      }
+    })
+  }
+} else {
+  for (let i = 0; i < workerData.incsPerThread; i++) {
+    await sequelize.query(`UPDATE MyInt SET i = i + 1`)
+  }
+  await sequelize.close();
+}
+
+})();
