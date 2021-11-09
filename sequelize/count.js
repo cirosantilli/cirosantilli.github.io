625ce8146df2657f2700c176a66882e3554934d6
Ciro Santilli <ciro.santilli@gmail.com>
2021-11-09 10:43:04 +0000

    illustrate more aggregation features in the count query

--- sequelize/count.js
+++ sequelize/count.js
@@ -16,19 +16,29 @@ const UserLikesPost = sequelize.define('UserLikesPost', {
 await UserLikesPost.sync({force: true})
 await UserLikesPost.create({userId: 1, postId: 1})
 await UserLikesPost.create({userId: 1, postId: 2})
+await UserLikesPost.create({userId: 1, postId: 3})
+await UserLikesPost.create({userId: 2, postId: 1})
 await UserLikesPost.create({userId: 2, postId: 2})
+await UserLikesPost.create({userId: 3, postId: 1})
+await UserLikesPost.create({userId: 4, postId: 1})
+// Count likes on all posts but:
+// - don't consider likes userId 4
+// - only return posts that have at least 2 likes
+// Order posts by those with most likes first.
 const postLikeCounts = await UserLikesPost.findAll({
   attributes: [
     'postId',
     [sequelize.fn('COUNT', '*'), 'count'],
   ],
   group: ['postId'],
+  where: { userId: { [Op.ne]: 4 }},
   order: [[sequelize.col('count'), 'DESC']],
+  having: sequelize.where(sequelize.fn('COUNT', '*'), Op.gte, 2)
 })
-assert.strictEqual(postLikeCounts[0].postId, 2)
-assert.strictEqual(parseInt(postLikeCounts[0].get('count'), 10), 2)
-assert.strictEqual(postLikeCounts[1].postId, 1)
-assert.strictEqual(parseInt(postLikeCounts[1].get('count'), 10), 1)
+assert.strictEqual(postLikeCounts[0].postId, 1)
+assert.strictEqual(parseInt(postLikeCounts[0].get('count'), 10), 3)
+assert.strictEqual(postLikeCounts[1].postId, 2)
+assert.strictEqual(parseInt(postLikeCounts[1].get('count'), 10), 2)
 assert.strictEqual(postLikeCounts.length, 2)
 await sequelize.close()
 })()
