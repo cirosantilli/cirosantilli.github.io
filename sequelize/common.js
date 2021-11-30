f9a2322c89ca90f7ee5739fdee85c70d40618655
Ciro Santilli <ciro.santilli@gmail.com>
2021-11-30 16:57:45 +0000

    sequelize/raw/parallel_select_and_update.js

--- sequelize/common.js
+++ sequelize/common.js
@@ -47,3 +47,65 @@ function sequelize(filename, dialect, opts) {
   }
 }
 exports.sequelize = sequelize
+
+// Get n sequelize instances for parallel tests.
+// Each one prepends its index to every log message.
+function sequelizes(n, filename, dialect, opts) {
+  return Array.from(
+    Array(n).keys(),
+    i => sequelize(
+      filename,
+      dialect,
+      { logging: (s) => console.log(`${i}: ${s}`) }
+    )
+  )
+}
+exports.sequelizes = sequelizes
+
+
+async function transaction(sequelize, isolation, cb) {
+  let done = false
+  if (isolation === undefined) {
+    isolation = 'SERIALIZABLE'
+  }
+  while (!done) {
+    if (isolation !== 'NONE') {
+      if (sequelize.options.dialect === 'sqlite') {
+        await sequelize.query(`BEGIN TRANSACTION`)
+      } else {
+        await sequelize.query(`BEGIN TRANSACTION ISOLATION LEVEL ${isolation}`)
+      }
+    }
+    try {
+      await cb(sequelize)
+      if (isolation !== 'NONE') {
+        await sequelize.query(`COMMIT`)
+      }
+      done = true;
+    } catch (e) {
+      if (
+        sequelize.options.dialect === 'postgres' &&
+        // This can happen randomly, and we have to re-run the transaction:
+        // - could not serialize access due to read/write dependencies among transactions
+        // - could not serialize access due to concurrent update
+        // https://www.postgresql.org/docs/13/errcodes-appendix.html
+        e.original.code === '40001'
+      ) {
+        if (isolation !== 'NONE') {
+          await sequelize.query(`ROLLBACK`)
+          // COMMIT would also work here in PostgreSQL it seems, when it enters error state it ignores
+          // everything until the transaction finishes, and the COMMIT then becomes as ROLLBACK:
+          // https://stackoverflow.com/questions/27245101/why-should-we-use-rollback-in-sql-explicitly/27245234#27245234
+          // but can't find any clear docs on it, this one:
+          // https://stackoverflow.com/questions/48277519/how-to-use-commit-and-rollback-in-a-postgresql-function/48277708#48277708
+          // points to some possible docs, but not very direct.
+          // await sequelize.query(`COMMIT`)
+        }
+      } else {
+        // Error that we don't know how to handle.
+        throw e;
+      }
+    }
+  }
+}
+exports.transaction = transaction
