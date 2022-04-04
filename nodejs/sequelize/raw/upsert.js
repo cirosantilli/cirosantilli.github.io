d0c5e8e47d741641795b3efc5b66614b7f580a77
Ciro Santilli <ciro.santilli@gmail.com>
2022-04-04 08:48:32 +0100

    master sql upsert

--- nodejs/sequelize/raw/upsert.js
+++ nodejs/sequelize/raw/upsert.js
@@ -6,44 +6,69 @@ const sequelize = common.sequelize(__filename, process.argv[2])
 
 // Create tables and data.
 await common.drop(sequelize, `Integer`)
+let autoincrement
+if (sequelize.options.dialect === 'sqlite') {
+  autoincrement = 'INTEGER PRIMARY KEY AUTOINCREMENT'
+} else {
+  autoincrement = 'SERIAL PRIMARY KEY'
+}
 await sequelize.query(`
 CREATE TABLE "Integer" (
-  id INTEGER PRIMARY KEY,
+  id ${autoincrement},
   value INTEGER NOT NULL UNIQUE,
   name TEXT NOT NULL,
-  inverse INTEGER NOT NULL
+  inverse INTEGER
 )
 `)
-await sequelize.query(`CREATE INDEX "valueIdx" ON "Integer" (value)`)
 async function reset() {
   await sequelize.query(`DELETE FROM "Integer"`)
   return sequelize.query(`
-INSERT INTO "Integer" VALUES
-(1, 2, 'two', -2),
-(2, 3, 'three', -3),
-(3, 5, 'five', -5)
+INSERT INTO "Integer" ("value", "name", "inverse") VALUES
+(2, 'two',   -2),
+(3, 'three', -3),
+(5, 'five',  -5)
 `)
 }
 await reset()
 
 let rows, meta
 
-// SELECT all.
+// Initial state.
 ;[rows, meta] = await sequelize.query(`SELECT * FROM "Integer" ORDER BY value ASC`)
 common.assertEqual(rows, [
   { id: 1, value: 2, name: 'two',   inverse: -2 },
   { id: 2, value: 3, name: 'three', inverse: -3 },
   { id: 3, value: 5, name: 'five',  inverse: -5 },
 ])
-;[rows, meta] = await sequelize.query(`INSERT INTO SELECT * FROM "Integer" ORDER BY value ASC`)
 
-const ret = await Integer.bulkCreate(
-  [
-    {value: 2, name: 'TWO'},
-    {value: 3, name: 'THREE'},
-    {value: 7, name: 'SEVEN'},
-  ],
-  { updateOnDuplicate: ["name"] }
-)
+if (sequelize.options.dialect !== 'sqlite') {
+  // Update.
+  // Doesn't work on sqlite3 node.js package latest version 5.0.2,
+  // which seems to prepack sqlite3 3.32. / But does work when running
+  // the query manually on Ubuntu 21.10, which has SQLite 3.35 (version
+  // in which RETURNING was implemented).
+  ;[rows, meta] = await sequelize.query(
+  `INSERT INTO "Integer" ("value", "name") VALUES
+(2,'TWO'),
+(3,'THREE'),
+(7,'SEVEN')
+ON CONFLICT ("value") DO UPDATE SET "name"=EXCLUDED."name"
+RETURNING "id", "value", "name", "inverse"
+`)
+  common.assertEqual(rows, [
+    { id: 1, value: 2, name: 'TWO',    inverse: -2 },
+    { id: 2, value: 3, name: 'THREE',  inverse: -3 },
+    { id: 6, value: 7, name: 'SEVEN',  inverse: null },
+  ])
+
+  // Final state.
+  ;[rows, meta] = await sequelize.query(`SELECT * FROM "Integer" ORDER BY value ASC`)
+  common.assertEqual(rows, [
+    { id: 1, value: 2, name: 'TWO',    inverse: -2 },
+    { id: 2, value: 3, name: 'THREE',  inverse: -3 },
+    { id: 3, value: 5, name: 'five',   inverse: -5 },
+    { id: 6, value: 7, name: 'SEVEN',  inverse: null },
+  ])
+}
 
 })().finally(() => { return sequelize.close() });
