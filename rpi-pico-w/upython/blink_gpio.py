7e93b00916f46af999fc29f5070a545134f80644
Ciro Santilli <ciro.santilli@gmail.com>
2022-09-03 12:55:52 +0000

    bak

--- rpi-pico-w/upython/blink_gpio.py
+++ rpi-pico-w/upython/blink_gpio.py
@@ -2,9 +2,11 @@ import machine
 import time
 
 led = machine.Pin('LED', machine.Pin.OUT)
-# For Rpi Pico (non-W) it was like this instead apparently.
-# led = Pin(25, Pin.OUT)
-
+pin = machine.Pin(0, machine.Pin.OUT)
+i = 0
 while (True):
+    pin.toggle()
     led.toggle()
+    print(i)
     time.sleep(.5)
+    i += 1
