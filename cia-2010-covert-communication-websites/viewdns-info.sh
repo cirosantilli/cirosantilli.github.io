94af5b44517a3c95042c9d5e7f4a7787501e6aa2
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2022-09-05 09:57:06 +0000

    bak

--- cia-2010-covert-communication-websites/viewdns-info.sh
+++ cia-2010-covert-communication-websites/viewdns-info.sh
@@ -2,7 +2,7 @@
 #APIKEY=98e6028acec4f0c4b147b839e54c3a957e24b671
 set -eu
 
-#n=20
+n=20
 #'iraniangoals.com 69.65.33 21' \
 #'iraniangoalkicks.com 68.178.232 100' \
 #'activegaminginfo.com 66.175.106 148' \
@@ -12,13 +12,20 @@ set -eu
 #'rastadirect.net 68.178.232 100' \
 #'fightwithoutrules.com 212.4.17 38'
 
-n=10
+#n=10
 #'fitness-dawg.com 219.90.62 210'
 #'fightwithoutrules.com 212.4.17 69'
 #'activegaminginfo.com 66.175.106 168'
 
 for line in \
-  'activegaminginfo.com 66.175.106 168'
+  'iraniangoals.com 69.65.33 21' \
+  'iraniangoalkicks.com 68.178.232 100' \
+  'activegaminginfo.com 66.175.106 148' \
+  'capture-nature.com 65.61.127 163' \
+  'headlines2day.com 118.139.174 1' \
+  'fitness-dawg.com 219.90.62 243' \
+  'rastadirect.net 68.178.232 100' \
+  'fightwithoutrules.com 212.4.17 38'
 do
   a=( $line )
   domain=${a[0]}
@@ -30,7 +37,7 @@ do
     ip_end_new=$(($ip_end + $i))
     if [ $ip_end_new -ge 0 ] && [ $ip_end_new -le 255 ]; then
       ip2="$ip_base.$ip_end_new"
-      echo curl --silent "https://api.viewdns.info/reverseip/?host=$ip2&apikey=$APIKEY&output=json"
+      #echo curl --silent "https://api.viewdns.info/reverseip/?host=$ip2&apikey=$APIKEY&output=json"
       curl --silent "https://api.viewdns.info/reverseip/?host=$ip2&apikey=$APIKEY&output=json" | jq -r '.response | select(.domains != null) | .domains[] | .name + " " + .last_resolved' | \
         while IFS="" read -r p || [ -n "$p" ]; do
           echo "* $i $ip2: $p"
