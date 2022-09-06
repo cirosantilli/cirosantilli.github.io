83c0cf65387539578d26cab6eb3198beab6cbcb8
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2022-09-06 11:57:32 +0000

    bak

--- cia-2010-covert-communication-websites/cdx.sh
+++ cia-2010-covert-communication-websites/cdx.sh
@@ -8,7 +8,7 @@ rm -f "$out_err"
 i=0
 while IFS= read -r domain; do
   echo $i $domain
-  curl --connect-timeout 5 "https://web.archive.org/cdx/search/cdx?url=$domain&matchType=domain&filter=urlkey:.*\.(cgi|jar|swf)&to=20140101000000&limit=5" | tee -a "$out"
+  curl --connect-timeout 5 "https://web.archive.org/cdx/search/cdx?url=$domain&matchType=domain&filter=urlkey" | tee -a "$out"
   if [ "${PIPESTATUS[0]}" -ne 0 ]; then
     echo $i $domain >> "$out_err"
   fi
