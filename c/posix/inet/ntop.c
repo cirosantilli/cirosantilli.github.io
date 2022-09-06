8604f72b5056f617cf80d25b347e898bb42b30e9
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2022-09-06 09:57:30 +0000

    bak

--- c/posix/inet/ntop.c
+++ c/posix/inet/ntop.c
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    char str[INET6_ADDRSTRLEN];
+    char str[INET_ADDRSTRLEN];
     struct in_addr addr;
 
     if (argc != 2) {
         exit(EXIT_FAILURE);
     }
     addr.s_addr = ntohl(strtoul(argv[1], NULL, 0));
-    printf("%s\n", inet_ntop(AF_INET, &addr, str, INET6_ADDRSTRLEN));
+    printf("%s\n", inet_ntop(AF_INET, &addr, str, INET_ADDRSTRLEN));
 }
--- sqlite/ip.c
+++ sqlite/ip.c
@@ -19,18 +19,16 @@ static void str2ipv4(
   const unsigned char *zIn;
   struct in_addr addr;
   int s;
-  unsigned char *zToFree = 0;
+
   assert( argc==1 );
   if( sqlite3_value_type(argv[0])==SQLITE_NULL ) return;
   zIn = (const unsigned char*)sqlite3_value_text(argv[0]);
-
   s = inet_pton(AF_INET, (const char * restrict)zIn, &addr);
   if (s <= 0) {
     /* TODO report error nicely. */
   }
   /* Need int64 otherwise overflows int32_t. There is no unsigned version apparently. */
   sqlite3_result_int64(context, ntohl(addr.s_addr));
-  sqlite3_free(zToFree);
 }
 
 static void ipv42str(
@@ -38,25 +36,15 @@ static void ipv42str(
   int argc,
   sqlite3_value **argv
 ){
-  const unsigned char *zIn;
   struct in_addr addr;
-  int s;
-  unsigned char *zToFree = 0;
+  char zOut[INET_ADDRSTRLEN];
+
   assert( argc==1 );
   if( sqlite3_value_type(argv[0])==SQLITE_NULL ) return;
-  zIn = (const unsigned char*)sqlite3_value_text(argv[0]);
-
-  s = inet_pton(AF_INET, (const char * restrict)zIn, &addr);
-  if (s <= 0) {
-    /* TODO report error nicely. */
-  }
-  /* Need int64 otherwise overflows int32_t. There is no unsigned version apparently. */
-  sqlite3_result_int64(context, ntohl(addr.s_addr));
-
-    addr.s_addr = ntohl(strtoul(argv[1], NULL, 0));
-    printf("%s\n", inet_ntop(AF_INET, &addr, str, INET6_ADDRSTRLEN));
-
-  sqlite3_free(zToFree);
+  addr.s_addr = ntohl(sqlite3_value_int64(argv[0]));
+  inet_ntop(AF_INET, &addr, zOut, INET_ADDRSTRLEN);
+  /* TODO any way to avoid the strlen here? inet_ntop does not seem to return it. */
+  sqlite3_result_text(context, (char*)zOut, strlen(zOut), SQLITE_TRANSIENT);
 }
 
 int sqlite3_ip_init(
@@ -70,7 +58,7 @@ int sqlite3_ip_init(
   rc = sqlite3_create_function(db, "str2ipv4", 1,
                    SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
                    0, str2ipv4, 0, 0);
-  if (rc = 0) {
+  if (rc == 0) {
     rc = sqlite3_create_function(db, "ipv42str", 1,
                     SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
                     0, ipv42str, 0, 0);
