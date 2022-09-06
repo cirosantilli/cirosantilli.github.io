7de6c8ab92e339b15ec1e1e9d58efba27d66a370
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2022-09-06 12:57:33 +0000

    bak

--- cia-2010-covert-communication-websites/cdx.sh
+++ cia-2010-covert-communication-websites/cdx.sh
@@ -3,14 +3,21 @@
 domains=$1
 out="$domains.cdx"
 out_err="$domains.cdx.err"
-rm -f "$out"
-rm -f "$out_err"
-i=0
-while IFS= read -r domain; do
+nfile="$domains.cdx.n"
+#rm -f "$out"
+#rm -f "$out_err"
+#rm -f "$nfile"
+if [ -r "$nfile" ]; then
+  i="$(cat "$nfile")"
+else
+  i=0
+fi
+tail -n+$((i + 1)) "$domains" | while IFS= read -r domain; do
   echo $i $domain
-  curl --connect-timeout 5 "https://web.archive.org/cdx/search/cdx?url=$domain&matchType=domain&filter=urlkey" | tee -a "$out"
+  curl --connect-timeout 5 "https://web.archive.org/cdx/search/cdx?url=$domain&matchType=domain&filter=urlkey:.*\.(cgi|jar|swf)&to=20140101000000&limit=5" | tee -a "$out"
   if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-    echo $i $domain >> "$out_err"
+    echo $domain >> "$out_err"
   fi
+  echo "$i" > "$nfile"
   i=$((i+1))
-done <$1
+done
