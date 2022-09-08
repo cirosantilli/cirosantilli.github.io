f6a67969c1aab589dfad6330e628ecf407a8c2c1
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2022-09-08 21:58:30 +0000

    bak

--- cia-2010-covert-communication-websites/hupo.sh
+++ cia-2010-covert-communication-websites/hupo.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 set -eu
 dir=tmp/hupo
-d="$(ls | tail -n1 "$dir")"
+mkdir -p "$dir"
+d="$(ls "$dir" | tail -n1)"
 if [ -z "$d" ]; then
   d=2012-01-01
+else
+  d="$(date -I -d "$d + 1 day")"
 fi
-mkdir -p "$dir"
-while [ "$d" != 2013-01-01 ]; do
+while [ "$d" != 2017-01-01 ]; do
   echo $d
   #sudo killall -HUP tor
   #curl --socks5 127.0.0.1:9050 "http://static.hupo.com/expdomain_myadmin/$d（国际域名）%20.txt" > "$dir/$d"
-  curl "http://static.hupo.com/expdomain_myadmin/$d（国际域名）%20.txt" > "$dir/$d"
-  d=$(date -I -d "$d + 1 day")
-  sleep 300
+  # Since ~2012-02-06 no space.
+  set +e
+  code="$(curl "http://static.hupo.com/expdomain_myadmin/$d（国际域名）.txt" -o "$dir/$d" -w "%{http_code}")"
+  stat=$?
+  set -e
+  if [ "$code" -eq 200 ]; then
+    echo sleep
+    # Good.
+    #sleep 300
+    sleep 180
+  fi
+  if [ "$stat" -eq 0 ]; then
+    d=$(date -I -d "$d + 1 day")
+  else
+    rm -f "$d"
+  fi
+  #curl "http://static.hupo.com/expdomain_myadmin/$d（国际域名）%20.txt" > "$dir/$d"
 done
--- cia-2010-covert-communication-websites/webmastercn.sh
+++ cia-2010-covert-communication-websites/webmastercn.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 set -eu
-d=2012-01-01
-dir=tmp/webmastercn
+dir=tmp/webmasterhome
 mkdir -p "$dir"
-while [ "$d" != 2013-01-01 ]; do
+d="$(ls "$dir" | tail -n1)"
+if [ -z "$d" ]; then
+  d=2012-01-01
+else
+  d="$(date -I -d "$d + 1 day")"
+fi
+while [ "$d" != 2017-01-01 ]; do
   echo $d
-  curl "http://domain.webmasterhome.cn/com/$d.asp" > "$dir/$d"
-  d=$(date -I -d "$d + 1 day")
+  #sudo killall -HUP tor
+  #curl --socks5 127.0.0.1:9050 "http://static.hupo.com/expdomain_myadmin/$d（国际域名）%20.txt" > "$dir/$d"
+  # Since ~2012-02-06 no space.
+  set +e
+  code="$(curl "http://domain.webmasterhome.cn/com/$d.asp" -o "$dir/$d" -w "%{http_code}")"
+  stat=$?
+  set -e
+  if [ "$code" -eq 200 ]; then
+    echo sleep
+    # Good.
+    #sleep 300
+    sleep 180
+  fi
+  if [ "$stat" -eq 0 ]; then
+    d=$(date -I -d "$d + 1 day")
+  else
+    rm -f "$d"
+  fi
+  #curl "http://static.hupo.com/expdomain_myadmin/$d（国际域名）%20.txt" > "$dir/$d"
 done
