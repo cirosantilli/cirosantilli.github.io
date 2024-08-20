9ac9227d970a7b180a5795aa4e44f35764732e6f
Ciro Santilli <cirosantilli@proton.me>
2024-08-20 12:35:34 +0100

    bak

---
+++ nodejs/next/ref-twice/pages/index.js
@@ -0,0 +1,28 @@
+import { useState, useRef, useEffect } from 'react'
+
+function complexExternalLibraryFunctionWithoutCleanup(elem, cb) {
+  elem.addEventListener('click', () => {
+    cb()
+  })
+}
+
+export default function IndexPage() {
+  const [text, setText] = useState('')
+  const [myint, setMyint] = useState(0)
+  const ref = useRef(null)
+  function incIt() {
+    setMyint(i => i+1)
+  }
+  useEffect(() => {
+    if (ref.current) {
+      complexExternalLibraryFunctionWithoutCleanup(ref.current, incIt)
+    }
+  }, [])
+  return <div>
+    <div><input value={text} onChange={e => setText(e.target.value)} placeholder='Type here' /></div>
+    <div>You typed: {text}</div>
+    <div><button ref={ref}>Click me after typing something! If unfixed, it will increment twice!</button></div>
+    <div><button onClick={incIt}>Click me to increment!</button></div>
+    <div>{myint}</div>
+  </div>
+}
