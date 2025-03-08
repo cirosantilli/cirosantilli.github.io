138e78c2158a7bcf016dc1b65db4970b75d77e2b
Ciro Santilli <cirosantilli@proton.me>
2025-03-08 14:47:33 +0000

    automatic source code refactor!

--- cia-2010-covert-communication-websites/hupo-merge.sh
+++ cia-2010-covert-communication-websites/hupo-merge.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
+
+# Input:
+#
+# tmp/hupo/yyyy-mm-dd
+# tmp/webmasterhome/yyyy-mm-dd
+# tmp/justdropped-post/yyyy-mm-dd
+#
+# Output:
+#
+# tmp/merge/yyyy-mm-dd
+
 set -eu
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 hupo_dir="${script_dir}/tmp/hupo"
 webmasterhome_dir="${script_dir}/tmp/webmasterhome"
-justdropped_dir="${script_dir}/tmp/justdropped"
+justdropped_dir="${script_dir}/tmp/justdropped-post"
 outdir="${script_dir}/tmp/merge"
 rm -rf "$outdir"
 mkdir -p "$outdir"
 if [ $# -gt 0 ]; then
   d="$1-01-21"
 else
-  d=2011-07-29
+  d=2006-01-01
 fi
 to="$(ls -1 "$hupo_dir" | tail -n1)"
 to2="$(ls -1 "$webmasterhome_dir" | tail -n1)"
-while [ "$d" \< "$to" ] || [ "$d" \< "$to2" ]; do
+to3="$(ls -1 "$justdropped_dir" | tail -n1)"
+while \
+  [ "$d" \< "$to" ] ||
+  [ "$d" \< "$to2" ] ||
+  [ "$d" \< "$to3" ]
+do
   echo "$d"
   (
     # dos2unic:
@@ -33,8 +49,8 @@ while [ "$d" \< "$to" ] || [ "$d" \< "$to2" ]; do
     if [ -f "$inf" ]; then
       "${script_dir}/webmastercn-post.sh" "$inf"
     fi
-    if [ -f "$justdropped_dir/$d-com.html" ]; then
-      "${script_dir}/justdropped-post.sh" "$justdropped_dir/$d"-*.html
+    if [ -f "$justdropped_dir/$d" ]; then
+      cat "$justdropped_dir/$d"
     fi
   ) | sort | uniq > "$outdir/$d"
   d=$(date -I -d "$d + 1 day")
--- cia-2010-covert-communication-websites/justdropped-post.sh
+++ cia-2010-covert-communication-websites/justdropped-post.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-  grep -Eh '^[^ ]+\.[^ ]+<br>$' "$@" | sed 's/<br>$//' | sort -o "$outd/$d"
+set -eu
+ind=tmp/justdropped
+ind=tmp/justdropped-post
+mkdir -p  "$outd"
+ls "$ind" | grep -- -com.html | while read com; do
+  d="${com%-com.*}"
+  echo $d
+  grep -Eh '^[^ ]+\.[^ ]+<br>$' "$ind/$d"*.html | sed 's/<br>$//' | sort -o "$outd/$d"
+done
