b41541fb29877795cb09cf172d6f5d4db86a7c8d
Ciro Santilli <cirosantilli@proton.me>
2025-05-30 15:34:13 +0100

    bak

--- cia-2010-covert-communication-websites/download-websites.sh
+++ cia-2010-covert-communication-websites/download-websites.sh
@@ -11,12 +11,13 @@ while read u; do
   # These have some large legit websites on the selected timerange which
   # we'd rather exclude to speed things up and reduce noise.
   case "$domain" in
+    altworldnews.com) f=20090101000000 ;;
     azerinews.org) t=20120101000000 ;;
     bcenews.com) f=20050101000000 ;;
     globaltourist.net) t=20120401000000 ;;
     inside-vc.com) f=20050101000000 ;;
     inews-today.com) t=20130601000000 ;;
-    www.cultura-digital.net) f=20060101000000
+    www.cultura-digital.net) f=20060101000000 ;;
     www.headlines2day.com) t=20120101000000 ;;
   esac
   if ! [ -d "websites/$domain" ]; then
